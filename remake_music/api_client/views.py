b3142e2f856fcbb28e1ad151668e60358b66ffd5
Alexei Tolkun <niree.xd@gmail.com>
2018-03-03 17:35:50 +0300

    create new base app for base structures, refactor viewsets to readonly viewsets in client api, some minor code cleans

--- remake_music/api_client/views.py
+++ remake_music/api_client/views.py
@@ -2,8 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics, permissions, viewsets
 
 from remake_music.album.models import Album
-from remake_music.api_client.permissions import (OnlyUserCanOrReadOnly,
-                                                 OnlyAnonUserCan)
+from remake_music.base import permissions as base_permissions
 from remake_music.artist.models import Artist
 from remake_music.track.models import Track
 from remake_music.like.models import Like
@@ -14,28 +13,29 @@ from remake_music.api_client.serializers import (TrackGeneralSerializer,
                                                  AlbumGeneralSerializer)
 
 
-class TrackViewSet(viewsets.ModelViewSet):
+class TrackViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Track.objects.all()
     serializer_class = TrackGeneralSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
 
-class ArtistViewSet(viewsets.ModelViewSet):
+class ArtistViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Artist.objects.prefetch_related('album_set').all()
     serializer_class = ArtistGeneralSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
 
-class AlbumViewSet(viewsets.ModelViewSet):
+class AlbumViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Album.objects.all().select_related('artist')
     serializer_class = AlbumGeneralSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
 
-class LikeViewSet(viewsets.ModelViewSet):
+class LikeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeInfoSerializer
-    permission_classes = (permissions.IsAuthenticated, OnlyUserCanOrReadOnly)
+    permission_classes = (permissions.IsAuthenticated,
+                          base_permissions.OnlyUserCanOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -47,10 +47,10 @@ class LikeViewSet(viewsets.ModelViewSet):
 class UserAPISignup(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (OnlyAnonUserCan,)
+    permission_classes = (base_permissions.OnlyAnonUserCan,)
 
 
 class UserAPIUpdateDetail(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (OnlyUserCanOrReadOnly,)
+    permission_classes = (base_permissions.OnlyUserCanOrReadOnly,)
